abc978f08327a0ca601fb89c72958a15de558d59
vijayGaikwad <vijay.gaikwad@thoughtworks.com>
2024-04-18 14:07:41 +0530

    sample input and output

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -9,3 +9,31 @@ class DemoApplication
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
+
+
+/*
+*
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("test.txt"));
+
+            bufferedWriter.write(String.valueOf(n));
+            bufferedWriter.newLine();
+
+
+            List<Integer> l =  Stream.of(br.readLine().trim().split("\\s+")).map(Integer::parseInt).collect(toList());
+
+            for (int i: l){
+                bufferedWriter.write(String.valueOf(i));
+                bufferedWriter.newLine();
+            }
+
+            bufferedWriter.flush();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        *
+* */
